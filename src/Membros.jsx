dff979dd819063781f3464f37006d56cad52eb57
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-12-09 12:38:08 -0300

    add texture in Member page

--- src/Membros.jsx
+++ src/Membros.jsx
@@ -1,9 +1,10 @@
+import PaperTexture from './assets/paper_texture.jpg'
+
 function Membros(){
 
-
     return(
-        <div className="conteiner p-10">
-            <h1 className="text-5xl text-center font-mono">Integrantes da Protocolor</h1>
+        <div className="conteiner p-10" id="contMebros">
+            <h1 className="text-3xl sm:text-5xl text-center font-mono">Integrantes da Protocolor</h1>
         </div>
     )
 }
