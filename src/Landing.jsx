e7ca4b8f3b8a4154e753d77865754301248610fd
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-12-09 02:28:15 -0300

    add margin top on protocolor h1

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,7 +6,7 @@ function Landing (){
         <div className="flex conteiner flex-col items-center bg-gradient-to-br from-white to-yellow-100">
                 <h1 className="text-5xl font-mono text-center mt-10 tracking-tigh">Seja bem vindo(a) ao Website da</h1>
                 <img src={LogoPC} style={{width:300}} className="mt-10" />
-                <h1 className="sm:text-9xl text-5xl tracking-tight font-semibold hover:tracking-wider transition-all duration-500 cursor-pointer">Protocolor</h1>
+                <h1 className="sm:text-9xl sm:mt-0 mt-5 text-5xl tracking-tight font-semibold hover:tracking-wider transition-all duration-500 cursor-pointer">Protocolor</h1>
         </div>
     )
 }
